bbc788960271805108aebd1c601c579d9feb0edc
Phillip Kravtsov <phillip@kravtsov.net>
2024-01-05 07:39:37 +0000

    wip

--- scripts/speculate_7B.sh
+++ scripts/speculate_7B.sh
@@ -1,4 +1,4 @@
-export DRAFT_MODEL_REPO=PY007/TinyLlama/TinyLlama-1.1B-Chat-v1.0
+export DRAFT_MODEL_REPO=TinyLlama/TinyLlama-1.1B-Chat-v1.0
 export MODEL_REPO=meta-llama/Llama-2-7b-chat-hf
 # --profile '7b_speculative'
 python generate.py --draft_checkpoint_path checkpoints/$DRAFT_MODEL_REPO/model_int8.pth --checkpoint_path checkpoints/$MODEL_REPO/model_int8.pth \
